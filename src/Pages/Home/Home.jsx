a0e27a7f7219f91f4cd05e6588e6ee96f727b965
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-04-15 21:59:36 +0530

    home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,19 +2,20 @@ import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
 
-function Home() {
-  const [isOpen, setIsOpen] = useState(false);
+const Home = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
 
-  return (
-    <>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Header toggle={toggle} />
-    </>
-  );
-}
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Header toggle={toggle} />
+            <h1 className="text-red-500 text-8xl">Home</h1>
+        </>
+    );
+};
 
 export default Home;
