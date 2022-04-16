8364dc414edda84e09d4403c8f352d98b406c544
heshani9918 <it20606060@my.sliit.lk>
2022-04-17 02:11:03 +0530

    Implement inventoryList page

---
+++ src/Pages/Inventory/List/index.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import InventoryTable from "../../../components/InventoryTable";
+import Header from "../../../components/Header/Header";
+import Sidebar from "../../../components/Sidebar/Sidebar";
+
+const InventoryList = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Header toggle={toggle} />
+            <div>
+            <h1 className="text-black font-bold px-10 pt-5 mt-4 mb-6 text-5xl">
+                Inventory
+            </h1>
+                <InventoryTable/>
+            </div>
+            
+        </>
+    );
+};
+
+export default InventoryList;
