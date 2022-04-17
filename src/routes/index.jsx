aaf6dd09455d03c6354652597c7f4cd588c3f12b
Poorna Sankalana <sanka2016pps@gmail.com>
2022-04-17 15:35:12 +0530

    adding student list route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ExamList, Home } from "../Pages";
+import { ExamList, Home, StudentList } from "../Pages";
 export const PageRoute = () => {
     return (
         <div>
@@ -9,6 +9,10 @@ export const PageRoute = () => {
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/exams" element={<ExamList />} />
+                        <Route
+                            path="/studentlist"
+                            element={<StudentList />}
+                        />
                     </Routes>
                 </div>
             </BrowserRouter>
