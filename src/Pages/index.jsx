fbcedccc89cffab1a16e4a919ee7d23869834b02
Poorna Sankalana <sanka2016pps@gmail.com>
2022-04-17 15:34:02 +0530

    adding student list to pages

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,4 +1,5 @@
 import ExamList from "./Exams/List";
 import Home from "./Home/Home";
+import StudentList from "./Student/List";
 
-export { ExamList, Home };
+export { ExamList, Home, StudentList };
