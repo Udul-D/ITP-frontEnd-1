efc1786d58f10d8839e8ad6c208eb463b0330d43
Poorna Sankalana <sanka2016pps@gmail.com>
2022-04-17 17:42:12 +0530

    Exporting LoginPage

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -3,5 +3,13 @@ import EventList from "./Events/List";
 import Home from "./Home/Home";
 import StudentList from "./Student/List";
 import InventoryList from "./Inventory/List";
+import LoginForm from "./Inventory/List";
 
-export { ExamList, Home, StudentList, InventoryList, EventList };
+export {
+    ExamList,
+    Home,
+    StudentList,
+    InventoryList,
+    EventList,
+    LoginForm,
+};
