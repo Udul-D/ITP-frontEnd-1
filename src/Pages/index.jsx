4601aae1c3f638bae7c2d6e0a513e260bf11e1b5
DilshanLakindu <dilshanlakindu5@gmail.com>
2022-04-18 14:27:39 +0530

    rout

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -2,8 +2,12 @@ import ExamList from "./Exams/List";
 import EventList from "./Events/List";
 import Home from "./Home/Home";
 import InventoryList from "./Inventory/List";
+import RequestList from "./Teachers/Request/List";
 
 
-export { ExamList, Home, EventList };
-export { ExamList, Home, InventoryList };
+
+
+
+export { ExamList, Home, EventList,InventoryList,RequestList, };
+
 
