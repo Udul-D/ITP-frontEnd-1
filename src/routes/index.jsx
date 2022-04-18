67c4633c69918562f343035bbcc6445a10dee887
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-04-18 19:43:10 +0530

    implement routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ExamList, Home, EventList, InventoryList } from "../Pages";
+import {
+    ExamList,
+    Home,
+    EventList,
+    InventoryList,
+    EventAdminViewList,
+} from "../Pages";
 
 export const PageRoute = () => {
     return (
@@ -11,10 +17,8 @@ export const PageRoute = () => {
                         <Route path="/" element={<Home />} />
                         <Route path="/exams" element={<ExamList />} />
                         <Route path="/events" element={<EventList />} />
-                        <Route
-                            path="/inventory"
-                            element={<InventoryList />}
-                        />
+                        <Route path="/inventory" element={<InventoryList />}/>
+                        <Route path="/admin/events" element={<EventAdminViewList />}/>
                     </Routes>
                 </div>
             </BrowserRouter>
