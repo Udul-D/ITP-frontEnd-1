86256cd11512d00686308a9b59f511d829141b6c
DilshanLakindu <dilshanlakindu5@gmail.com>
2022-04-18 15:32:05 +0530

    merge changes

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,13 +1,20 @@
 import ExamList from "./Exams/List";
 import EventList from "./Events/List";
 import Home from "./Home/Home";
+import TeacherList from "../components/TeacherCard/TeacherList";
+import StudentList from "./Student/List";
 import InventoryList from "./Inventory/List";
-import RequestList from "./Teachers/Request/List";
+import LoginForm from "./Inventory/List";
+import Requests from "./Teachers/Request/List/index";
 
-
-
-
-
-export { ExamList, Home, EventList,InventoryList,RequestList, };
-
+export {
+    ExamList,
+    Home,
+    StudentList,
+    InventoryList,
+    EventList,
+    LoginForm,
+    TeacherList,
+    Requests,
+};
 
