acc53cf662aaa2768426cf88ec8bccfc02fbecb7
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-04-18 19:49:00 +0530

    implement index routes

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -2,11 +2,6 @@ import ExamList from "./Exams/List";
 import EventList from "./Events/List";
 import Home from "./Home/Home";
 import InventoryList from "./Inventory/List";
-import LoginForm from "./Login/index";
+import EventAdminViewList from "./EventAdmin/List";
 
-<<<<<<< HEAD
-export { ExamList, Home, EventList, InventoryList };
-=======
-export { ExamList, Home, InventoryList, EventList };
-
->>>>>>> 1187a76fead94a9c5465a4d8e665ab69b80bce01
+export { ExamList, Home, InventoryList, EventList, EventAdminViewList };
