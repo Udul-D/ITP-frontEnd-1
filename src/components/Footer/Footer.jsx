3614bfc1ab9adb54b39c038a9c0bed0738263b7f
“[Navodhya <navodhya2011@gmail.com>
2022-04-19 13:21:14 +0530

    Footer feedback width changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,11 +11,11 @@ const Footer = () => {
                     <span className="text-teal-400 ">Send</span> Your
                     Feedback for Us
                 </h1>
-                <div className="w-full">
+                <div className="w-full lg:mt-5">
                     <input
                         type="text"
                         placeholder="Enter Your Feedback"
-                        className="text-gray-800 sm:w-full sm:ml-0 lg:ml-7 sm:mr-5 w-full mb-4 py-2.5 lg:w-full lg:mr-10 lg:mb-5 rounded px-2 focus:outline-none md:ml-4"
+                        className="text-gray-800 sm:w-full sm:ml-0 lg:ml-7 sm:mr-5 w-full mb-4 py-2.5 lg:w-6/12 lg:mr-10 lg:mb-5 rounded px-2 focus:outline-none md:ml-4"
                     />
                     <button className="lg:ml-7 md:ml-4 sm:ml-0 bg-teal-600 text-white hover:bg-teal-700 duration-300 px-20 py-2.5 font-[Poppins] rounded-md md:w-auto w-full">
                         Send Feedback
