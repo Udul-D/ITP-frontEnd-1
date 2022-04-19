9e345b43277ec0b52fb1cdf2d9859e8c6b684eed
heshani9918 <it20606060@my.sliit.lk>
2022-04-19 23:52:41 +0530

    implement pages index

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -2,7 +2,8 @@ import ExamList from "./Exams/List";
 import EventList from "./Events/List";
 import Home from "./Home/Home";
 import InventoryList from "./Inventory/List";
-import LoginForm from "./Login/index";
+//import LoginForm from "./Login/index";
+import TimetableList from "./Timetable/List";
 
-export { ExamList, Home, InventoryList, EventList };
+export { ExamList, Home, InventoryList, EventList, TimetableList, };
 
