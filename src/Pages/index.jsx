03bbbf9326355b1d0caa2c8e2ec069db08298b23
Udul-D <uduldulsara@gmail.com>
2022-04-26 14:01:54 +0530

    Update index for admin list

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -7,7 +7,7 @@ import InventoryList from "./Inventory/List";
 import TimetableList from "./Timetable/List";
 import LoginPage from "./Login/index";
 import EventAdminViewList from "./EventAdmin/List";
-import LoginForm from "./Login/index";
+//import LoginForm from "./Login/index";
 import Requests from "./Teachers/Request/List";
 import HallList from "./Hall/List";
 import ResultList from "./Result/TeacherList/index";
@@ -15,8 +15,9 @@ import StudentResult from "./Result/StudentList/index";
 import RequestAdminViewList from "./Teachers/Request/List/AdminList/index";
 import TutorialList from "./Tutorial/List";
 import AddExam from "./Exams/add/index";
-import AddAdmin from "./Admin/AdminList/index";
+import AddAdmin from "./Admin/Add/index";
 import AddStudent from "./Student/add/index";
+import AdminList from "./Admin/AdminList/index";
 
 export {
     ExamList,
@@ -37,5 +38,6 @@ export {
     AddAdmin,
     TimetableList,
     AddStudent,
+    AdminList,
 };
 
