7efad57179eeda6be5cc1154a56ee314653c1c32
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-12 15:31:53 +0530

    define accress to create exams

--- src/Pages/Exams/List/index.jsx
+++ src/Pages/Exams/List/index.jsx
@@ -5,10 +5,12 @@ import Sidebar from "../../../components/Sidebar/Sidebar";
 import axios from "axios";
 import Footer from "../../../components/Footer/Footer";
 import Notification from "../../../components/Notification/index";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 const ExamList = () => {
     const [isOpen, setIsOpen] = useState(false);
     const location = useLocation();
+    const navigate = useNavigate();
+
     const toggle = () => {
         setIsOpen(!isOpen);
     };
@@ -29,13 +31,29 @@ const ExamList = () => {
         fetchExams();
     }, []);
 
+    const addExam = () => {
+        navigate("/teacher/exam/add");
+    };
+
     return (
         <>
             <Sidebar isOpen={isOpen} toggle={toggle} />
             <Header toggle={toggle} />
-            <h1 className="text-black font-bold px-10 pt-5 text-5xl">
-                Your Exams
-            </h1>
+            <div className="flex justify-between">
+                <h1 className="text-black font-bold px-10 pt-5 text-5xl">
+                    Your Exams
+                </h1>
+                {localStorage.getItem("role") === "teacher" ? (
+                    <button
+                        class="
+                    bg-green-600 hover:bg-green-800 text-white font-bold py-3 px-8 flex sm rounded-full mb-3 mr-10 mt-5"
+                        onClick={addExam}>
+                        Add New Exam
+                    </button>
+                ) : (
+                    <div></div>
+                )}
+            </div>
             <div className="w-full py-10 pl-28 gap-4 flex-wrap flex justify-start">
                 {exams.map((exam) => (
                     <ExamCard exam={exam} />
