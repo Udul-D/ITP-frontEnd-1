c78e5780ec009c4d1673be9824e4b5711ff3529e
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-05-16 18:45:14 +0530

    implement routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -24,6 +24,7 @@ import {
     AddStudent,
     AddTeacher,
     AddResult,
+    UpdateEvent,
 } from "../Pages";
 
 import LoginPage from "../Pages/Login";
@@ -123,6 +124,10 @@ export const PageRoute = () => {
                         <Route
                             path="/teacher/result/add"
                             element={<AddResult />}
+                        />
+                        <Route
+                            path="/admin/event/update/:id"
+                            element={<UpdateEvent />}
                         />
                     </Routes>
                 </div>
