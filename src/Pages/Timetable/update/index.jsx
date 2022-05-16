885ddae85525ed2d7a80ce0587ae81017baa37a7
heshani9918 <it20606060@my.sliit.lk>
2022-05-16 19:29:52 +0530

    Implement timetable update

---
+++ src/Pages/Timetable/update/index.jsx
@@ -0,0 +1,270 @@
+import React, { useEffect, useState } from "react";
+import Header from "../../../components/Header/Header";
+import Sidebar from "../../../components/Sidebar/Sidebar";
+import Footer from "../../../components/Footer/Footer";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
+import DatePicker from "react-datepicker";
+
+
+function UpdateTimetable() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const id = window.location.pathname.split("/")[4];
+    let navigate = useNavigate();
+
+
+    const [selectDate, setSelectDate] = useState(null);
+    const [subject, setSubject] = useState("");
+    const [grade, setGrade] = useState("");
+    const [teacherName, setTeacherName] = useState("");
+    const [hallNumber, setHallNumber] = useState("");
+    const [time, setTime] = useState("");
+    const [classType, setClassType] = useState("");
+    const [medium, setMedium] = useState("");
+    const [floorNumber, setFloorNumber] = useState("");
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const getData = async () => {
+                    setSelectDate(location.state.selectDate);
+                    setSubject(location.state.subject);
+                    setGrade(location.state.grade);
+                    setTeacherName(location.state.teacherName);
+                    setHallNumber(location.state.hallNumber);
+                    setTime(location.state.time);
+                    setClassType(location.state.classType);
+                    setMedium(location.state.medium);
+                    setFloorNumber(location.state.floorNumber);
+                };
+                getData();
+    }, [location]);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            subject: subject,
+            grade: grade,
+            date: selectDate,
+            teacherName: teacherName,
+            hallNumber: hallNumber,
+            time: time,
+            classType: classType,
+            medium: medium,
+            floorNumber: floorNumber,
+        };
+
+        console.log(id);
+
+        try {
+            await axios
+                .put("/api/timetable/update/" + id, {
+                    headers: {
+                        authToken: localStorage.getItem("authToken"),
+                    },
+                    data,
+                })
+                .then((res) => {
+                    console.log(res);
+                    navigate("/admin/timetable");
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Header toggle={toggle} />
+            <div className="text-center py-5">
+                <h1 className="font-bold text-5xl text-black">
+                    Update Timetable
+                </h1>
+            </div>
+            <div className="mx-96 w-1/2">
+                <div className="bg-gray-100 shadow-md rounded p-5 mb-10">
+                    <form className="bg-white rounded px-8 pt-6 pb-8 mb-4" autoComplete="off" onSubmit={onSubmit}>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Subject
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Subject"
+                                onChange={(e) =>
+                                    setSubject(e.target.value)}
+                                    value={subject}
+                            />
+                        </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Grade
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Grade"
+                                onChange={(e) =>
+                                    setGrade(e.target.value)}
+                                    value={grade}
+                            
+                            />
+                        </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Teacher Name
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Teacher Name"
+                                onChange={(e) =>
+                                    setTeacherName(e.target.value) }
+                                    value={teacherName}
+                            />
+                        </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Hall Number
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Hall Number"
+                                onChange={(e) =>
+                                    setHallNumber(e.target.value)}
+                                    value={hallNumber}
+                            />
+                            
+                        </div>
+                        <div class="mb-4">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Date
+                            </label>
+                            <div class="relative flex">
+                                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none z-10">
+                                <svg
+                                    class="w-5 h-5 text-gray-700 dark:text-gray-400 top-10"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                                </div>
+                                <DatePicker
+                                    className="shadow appearance-none border rounded w-full py-2 pr-3 pl-10 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                    selected={selectDate}
+                                    dateFormat="dd/MM/yyyy"
+                                    onChange={(e) =>
+                                        setSelectDate(e.target.value)}
+                                        value={selectDate}
+                                    
+                                    minDate={new Date()}                                
+                                />
+                            </div>
+                        </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Time
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Time"
+                                required
+                                onChange={(e) =>
+                                    setTime(e.target.value)}
+                                    value={time}
+                            />
+                        </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Class Type
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Class Type"
+                                onChange={(e) =>
+                                    setClassType(e.target.value)}
+                                    value={classType}
+                            />
+                        </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Medium
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Medium"
+                                onChange={(e) =>
+                                    setMedium(e.target.value)}
+                                    value={medium}
+                            />
+                        </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Floor Number
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Floor Number"
+                                onChange={(e) =>
+                                    setFloorNumber(e.target.value)}
+                                    value={floorNumber}
+                            />
+                        </div>
+                        
+                        <button class="bg-green-600 mx-48 mt-4 hover:bg-green-700 text-white font-bold py-2 px-24 rounded">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default UpdateTimetable;
