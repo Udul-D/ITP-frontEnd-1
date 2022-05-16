4c4d587dd944d016e0cb172b99036ec7e648795d
heshani9918 <it20606060@my.sliit.lk>
2022-05-16 19:25:49 +0530

    Implement add inventory table

--- src/components/InventoryTable/index.jsx
+++ src/components/InventoryTable/index.jsx
@@ -3,8 +3,31 @@ import {
     EditOutlined,
     DeleteOutlined,
 } from "@ant-design/icons";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { format } from 'date-fns';
+
 
 export default function Inventory() {
+
+    const [inventory, setInventory] = useState([]);
+
+    useEffect(() => {
+        const fetchInventory = async () => {
+            const res = await axios.get("/api/inventory/all");
+            setInventory(res.data);
+            console.log(res.data);
+        };
+        fetchInventory();
+    }, []);
+
+    const deleteInventory = async (id) => {
+        await axios.delete(`/api/inventory/delete/${id}`);
+        alert("deleted");
+        window.location.reload();
+        
+      }; 
+
     return (
         <div className="p-26">
             <div class=" items-center justify-center mb-10 bg-white">
@@ -13,7 +36,7 @@ export default function Inventory() {
                     <div class="p-5 bg-gray-100 rounded-lg ">
                         <div class="overflow-auto rounded-lg shadow">
                             <div>
-                                <a href="#">
+                                <a href="/admin/inventory/add">
                                     <button
                                         class="
                 bg-green-600
@@ -60,119 +83,48 @@ export default function Inventory() {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr class="bg-green-100 lg:text-black">
-                                        <td class="p-3">
-                                            <a href="#" class="mr-2">
-                                                <i class="material-icons-outlined text-base">
-                                                    <EyeOutlined
-                                                        className="text-gray-500
-                                                        mr-2
-                                                        hover:text-gray-800"
-                                                    />
-                                                </i>
-                                            </a>
-                                            <a href="#" class="mx-2">
-                                                <i class="material-icons-outlined text-base">
-                                                    <EditOutlined
-                                                        className="text-yellow-400
-                                                        mx-2
-                                                        hover:text-yellow-500"
-                                                    />
-                                                </i>
-                                            </a>
-                                            <a href="#" class="ml-2">
-                                                <i class="material-icons-round text-base">
-                                                    <DeleteOutlined className="text-red-400 ml-2 hover:text-red-500" />
-                                                </i>
-                                            </a>
-                                        </td>
-                                        <td class="p-3 font-medium capitalize">
-                                            White Board
-                                        </td>
-                                        <td class="p-3">2022/04/01</td>
-                                        <td class="p-3 uppercase pl-8">
-                                            2
-                                        </td>
-                                        <td class="p-3 uppercase">
-                                            9000.00
-                                        </td>
-                                    </tr>
+                                {inventory.map((r) => (
+                                        <tr class="bg-green-100 lg:text-black">
+                                            <td class="p-3">
+                                                <a
+                                                    href="#"
+                                                    class="text-gray-500 hover:text-gray-100 mr-2 px-2">
+                                                    <i class="material-icons-outlined text-base">
+                                                        <EyeOutlined />
+                                                    </i>
+                                                </a>
+                                                <a
+                                                    href="#"
+                                                    class="text-yellow-400 hover:text-gray-100 mx-2 px-2">
+                                                    <i class="material-icons-outlined text-base">
+                                                        <EditOutlined />
+                                                    </i>
+                                                </a>
+                                                <a
+                                                    href="#"
+                                                    class="text-red-400 hover:text-gray-100 ml-2 px-2">
+                                                    <i class="material-icons-round text-base">
+                                                        <DeleteOutlined onClick={() => deleteInventory(r._id)}/>
+                                                    </i>
+                                                </a>
+                                            </td>
+                                            <td class="p-3 font-medium capitalize">
+                                                {r.itemName}
+                                            </td>
+                                            <td class="p-3">
+                                                {r.boughtDate}
+                                              
 
-                                    <tr class="bg-green-100 lg:text-black">
-                                        <td class="p-3">
-                                            <a href="#" class="mr-2">
-                                                <i class="material-icons-outlined text-base">
-                                                    <EyeOutlined
-                                                        className="text-gray-500
-                                                        mr-2
-                                                        hover:text-gray-800"
-                                                    />
-                                                </i>
-                                            </a>
-                                            <a href="#" class="mx-2">
-                                                <i class="material-icons-outlined text-base">
-                                                    <EditOutlined
-                                                        className="text-yellow-400
-                                                        mx-2
-                                                        hover:text-yellow-500"
-                                                    />
-                                                </i>
-                                            </a>
-                                            <a href="#" class="ml-2">
-                                                <i class="material-icons-round text-base">
-                                                    <DeleteOutlined className="text-red-400 ml-2 hover:text-red-500" />
-                                                </i>
-                                            </a>
-                                        </td>
-                                        <td class="p-3 font-medium capitalize">
-                                            White Board
-                                        </td>
-                                        <td class="p-3">2022/04/01</td>
-                                        <td class="p-3 uppercase pl-8">
-                                            2
-                                        </td>
-                                        <td class="p-3 uppercase">
-                                            9000.00
-                                        </td>
-                                    </tr>
-
-                                    <tr class="bg-green-100 lg:text-black">
-                                        <td class="p-3">
-                                            <a href="#" class="mr-2">
-                                                <i class="material-icons-outlined text-base">
-                                                    <EyeOutlined
-                                                        className="text-gray-500
-                                                        mr-2
-                                                        hover:text-gray-800"
-                                                    />
-                                                </i>
-                                            </a>
-                                            <a href="#" class="mx-2">
-                                                <i class="material-icons-outlined text-base">
-                                                    <EditOutlined
-                                                        className="text-yellow-400
-                                                        mx-2
-                                                        hover:text-yellow-500"
-                                                    />
-                                                </i>
-                                            </a>
-                                            <a href="#" class="ml-2">
-                                                <i class="material-icons-round text-base">
-                                                    <DeleteOutlined className="text-red-400 ml-2 hover:text-red-500" />
-                                                </i>
-                                            </a>
-                                        </td>
-                                        <td class="p-3 font-medium capitalize">
-                                            White Board
-                                        </td>
-                                        <td class="p-3">2022/04/01</td>
-                                        <td class="p-3 uppercase pl-8">
-                                            2
-                                        </td>
-                                        <td class="p-3 uppercase">
-                                            9000.00
-                                        </td>
-                                    </tr>
+                                            </td>
+                                            <td class="p-3 uppercase pl-8">
+                                                {r.quantity}
+                                            </td>
+                                            <td class="p-3 uppercase">
+                                                {r.price}
+                                            </td>
+                                        </tr>
+                                    ))}
+                                    
                                 </tbody>
                             </table>
                         </div>
