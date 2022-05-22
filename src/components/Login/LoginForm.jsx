592b97fc0bf26586482325b4a47b2406c2bb288f
Poorna Sankalana <sanka2016pps@gmail.com>
2022-05-22 12:42:15 +0530

    setting id to a local storage

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -58,6 +58,11 @@ export default function LoginForm() {
                 localStorage.setItem("isLoggedIn", true);
                 localStorage.setItem("role", result.data.role);
                 localStorage.setItem("roleData", result.data.roleData);
+                localStorage.setItem("roleID", result.data.roleData._id);
+
+                console.log("RoleData" + localStorage.getItem("roleData"));
+
+                console.log("id" + localStorage.getItem("roleID"));
                 if (result.data.role === "teacher") {
                     localStorage.setItem(
                         "teacherName",
