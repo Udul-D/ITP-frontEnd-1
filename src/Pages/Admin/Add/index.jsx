bbc679276ba910478b19a2c30a16693778107f02
Udul-D <uduldulsara@gmail.com>
2022-05-22 12:40:32 +0530

    Delete Unnessary parts

--- src/Pages/Admin/Add/index.jsx
+++ src/Pages/Admin/Add/index.jsx
@@ -1,9 +1,9 @@
 import { Fragment, useState } from "react";
 import Header from "../../../components/Header/Header";
 import Sidebar from "../../../components/Sidebar/Sidebar";
-import "react-datepicker/dist/react-datepicker.css";
 import Footer from "../../../components/Footer/Footer";
 import axios from "axios";
+
 function AddAdmin() {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -64,22 +64,6 @@ function AddAdmin() {
                     </h1>
                 </div>
                 <form  onSubmit={onSubmit} className="bg-white rounded px-8 pt-6 pb-8 mb-8 shadow-md">
-                
-                    <div class="flex w-full items-center justify-center bg-grey-lighter">
-                        <label class="w-48 mb-6 flex flex-col items-center px-2 py-3 rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer bg-green-600 hover:bg-green-800 text-white hover:text-white">
-                            <svg
-                                class="w-8 h-8"
-                                fill="currentColor"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20">
-                                <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                            </svg>
-                            <span class="mt-2 text-base leading-normal">
-                                Select Your Photo
-                            </span>
-                            <input type="file" class="hidden" />
-                        </label>
-                    </div>
 
                     <div class="mb-6">
                         <label
