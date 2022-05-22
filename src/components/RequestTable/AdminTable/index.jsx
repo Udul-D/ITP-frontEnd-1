bc91bcc38891cdd03e7cb3c14ce6841897395a8c
DilshanLakindu <dilshanlakindu5@gmail.com>
2022-05-22 21:33:10 +0530

    Change Admin request table

--- src/components/RequestTable/AdminTable/index.jsx
+++ src/components/RequestTable/AdminTable/index.jsx
@@ -1,16 +1,28 @@
-import {
-    EyeOutlined,
-    EditOutlined,
-    DeleteOutlined,
-} from "@ant-design/icons";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
+import ConfirmDialog from "../../../components/ConfirmDialog/index";
+import Notification from "../../../components/Notification/index";
+import jsPDF from "jspdf";
+import Header from "../../Header/Header";
 import { Routes, Route } from "react-router-dom";
+import Footer from "../../Footer/Footer";
 
 export default function AdminRequestList() {
 
     const [AdminRequestList, setAdminRequestList] = useState([]);
+
+    const [notify, setNotify] = useState({
+        isOpen: false,
+        message: "",
+        type: "",
+    });
+
+    const [confirmDialog, setConfirmDialog] = useState({
+        isOpen: false,
+        title: "",
+        subTitle: "",
+    });
 
     useEffect(() => {
         const fetchAdminRequestList = async () => {
@@ -20,6 +32,27 @@ export default function AdminRequestList() {
         };
         fetchAdminRequestList();
     }, []);
+
+    const columns = [
+        { title: "Teacher Name", field: "teacherName" },
+        { title: "Request Title", field: "requestTitle" },
+        { title: "Date", field: "Date"},
+        { title: "Time", field: "time" },
+        { title: "Description", field: "description" },
+
+    ];
+    const downLoadPdf = () => {
+        const doc = new jsPDF();
+        doc.text("Teacher's Request Sheet",20,10);
+        doc.autoTable({
+            columns: columns.map((col) => ({
+                ...col,
+                dataKey: col.field,
+            })),
+            body: AdminRequestList,
+        });
+        doc.save(" Request Sheet");
+    };
 
     return (
         <div className="p-26">
@@ -41,61 +74,14 @@ export default function AdminRequestList() {
                 sm
                 rounded-full mb-3
                 
-            ">
-                                        All
+            "
+            onClick={() => downLoadPdf()}>
+                                        Download PDF
                                     </button>
                                 </a>
                             
 
 
-
-                            
-                                <a href="#">
-                                    <button
-                                        class="
-                
-                hover:bg-green-800
-                text-black
-                py-1
-                px-8
-                sm
-                rounded-full mb-3
-            ">
-                                        Upcoming
-                                    </button>
-                                </a>
-
-
-                                <a href="#">
-                                    <button
-                                        class="
-                
-                hover:bg-green-800
-                text-black
-                py-1
-                px-5
-                sm
-                rounded-full mb-3
-            ">
-                                        Past
-                                    </button>
-                                </a>
-
-
-                                <a href="#">
-                                    <button
-                                        class="
-                
-                hover:bg-green-800
-                text-black
-                py-1
-                px-5
-                sm
-                rounded-full mb-3
-            ">
-                                        Ongoing
-                                    </button>
-                                </a>
                             </div>
 
                             <table class="w-full">
@@ -126,7 +112,7 @@ export default function AdminRequestList() {
                                         
                                     <td class="p-3 font-medium capitalize">{r.teacherName}</td>
                                     <td class="p-3 font-medium capitalize">{r.requestTitle}</td>
-                                    <td class="p-3 font-medium capitalize">{r.Date}</td>
+                                    <td class="p-3 font-medium capitalize">{r.Date.split("T")[0]}</td>
                                     <td class="p-3 font-medium capitalize">{r.time}</td>
                                     <td class="p-3 font-medium capitalizee">{r.description}</td>
                                     </tr>
@@ -138,6 +124,11 @@ export default function AdminRequestList() {
                     </div>
                 </div>
             </div>
+            <Notification notify={notify} setNotify={setNotify}/>
+              <ConfirmDialog 
+                     confirmDialog={confirmDialog}
+                     setConfirmDialog={setConfirmDialog} />
+                     
         </div>
     );
 }
